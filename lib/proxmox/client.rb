fac42457f9a17d8bdddc9256cfdfffdff0f54206
Alex Wellnitz <moin@wellnitz-alex.de>
2025-09-30 16:13:58 +0200

    refactor: http client

--- lib/proxmox/client.rb
+++ lib/proxmox/client.rb
@@ -22,20 +22,53 @@ module Proxmox
     end
 
     def request(method, path, params = {}, body = nil)
-      response = http.send(method) do |req|
-        req.url "/api2/json#{path}"
-        req.headers["Cookie"] = "PVEAuthCookie=#{ticket}"
-        req.headers["CSRFPreventionToken"] = csrf_token if %i[post put delete].include?(method)
-        req.params.update(params) if method == :get
-        req.body = body.to_json if body
-      end
+      response = perform_http_call(method, path, params, body)
 
-      raise ApiError, response.body unless response.success?
-
-      JSON.parse(response.body)["data"]
+      ensure_success!(response)
+      extract_data(response)
     end
 
     private
+
+    def perform_http_call(method, path, params, body)
+      http.send(method) do |req|
+        set_url(req, path)
+        add_cookie_header(req)
+        set_csrf_header(req, method)
+        set_query_params(req, method, params)
+        set_body(req, body)
+      end
+    end
+
+    def set_url(req, path)
+      req.url "/api2/json#{path}"
+    end
+
+    def add_cookie_header(req)
+      req.headers["Cookie"] = "PVEAuthCookie=#{ticket}"
+    end
+
+    def set_csrf_header(req, method)
+      return unless %i[post put delete].include?(method)
+
+      req.headers["CSRFPreventionToken"] = csrf_token
+    end
+
+    def set_query_params(req, method, params)
+      req.params.update(params) if method == :get && params.any?
+    end
+
+    def set_body(req, body)
+      req.body = body.to_json if body
+    end
+
+    def ensure_success!(response)
+      raise ApiError, response.body unless response.success?
+    end
+
+    def extract_data(response)
+      JSON.parse(response.body)["data"]
+    end
 
     def http
       @http ||= Faraday.new(url: @base_url, ssl: { verify: @verify_ssl }) do |f|
