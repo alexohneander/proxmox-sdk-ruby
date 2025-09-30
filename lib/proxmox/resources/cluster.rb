ccce361c33e0b5ccbad0edbf7245c427c10edcd7
Alex Wellnitz <moin@wellnitz-alex.de>
2025-09-30 17:05:54 +0200

    feat: add cluster class functionallity

--- lib/proxmox/resources/cluster.rb
+++ lib/proxmox/resources/cluster.rb
@@ -8,6 +8,36 @@ module Proxmox
         @client = client
       end
 
+      def log(max: nil)
+        params = {}
+        params[:max] = max unless max.nil?
+
+        @client.request(:get, "/cluster/log", params)
+      end
+
+      def nextid(vmid: nil)
+        params = {}
+        params[:vmid] = vmid unless vmid.nil?
+
+        @client.request(:get, "/cluster/nextid", params)
+      end
+
+      def options
+        @client.request(:get, "/cluster/options")
+      end
+
+      def resources
+        @client.request(:get, "/cluster/resources")
+      end
+
+      def status
+        @client.request(:get, "/cluster/status")
+      end
+
+      def tasks
+        @client.request(:get, "/cluster/tasks")
+      end
+
       # Getting all Nodes
       def nodes
         @client.request(:get, "/nodes")
